c42e7b935b5d9228460b3850d55a6a3af86e6d55
AlexMureti <alexmkmaingi@gmail.com>
2025-09-13 13:26:35 +0300

    first class functions completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+//Assign functions to a variable
+//store functions in a data structure
+//write functions that reurn other functions
+//pass a function to another function
+//call a function return by another function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selector) {
+    return selector(drivers);
+}
